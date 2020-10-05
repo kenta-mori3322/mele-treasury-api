bc4dad62376b19a09470c72d2d614c4e119408c1
Vidor Gencel <vidorge@gmail.com>
2020-10-05 23:02:03 +0200

    Disburse schema

---
+++ src/models/disburse.js
@@ -0,0 +1,42 @@
+import { Schema, model } from 'mongoose'
+
+/**
+ * Disburse schema
+ */
+const disburseSchema = new Schema(
+    {
+        reference_id: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        amount: {
+            type: String,
+            required: true,
+        },
+        hash: {
+            type: String,
+            required: false,
+            default: '',
+        },
+    },
+    {
+        collection: 'disbursements',
+        timestamps: true,
+    }
+)
+
+class DisburseClass {}
+
+/** Add unique index */
+disburseSchema.index({ reference_id: 1 }, { unique: true })
+
+/** Add the class method maps to the schema method */
+disburseSchema.loadClass(DisburseClass)
+
+const Disburse = model('Disburse', disburseSchema)
+
+export default Disburse
