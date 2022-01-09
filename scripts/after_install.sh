d0b8447ef441f3904327fe9bf28076acc45cb6f7
iraklitchedia <iraklitchedia@gmail.com>
2022-01-09 17:19:59 +0400

    CI/CD setup

---
+++ scripts/after_install.sh
@@ -0,0 +1,16 @@
+sudo apt-get update
+sudo apt upgrade -y
+
+sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo apt-get install -y build-essential
+
+sudo apt install npm -y
+
+sudo npm -g install pm2
+sudo chmod -R 0755 /usr/lib/node_modules
+
+sudo apt install -y mongodb
+
+sudo cp .env mele-treasury-api-dev/.env
---
+++ scripts/application_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo cp /home/ubuntu/.env /home/ubuntu/mele-treasury-api-dev
+cd /home/ubuntu/mele-treasury-api-dev
+sudo npm run pm2:stop
+sudo npm run pm2:start
