f379d705acf35b0435d52ebb56f00d2691e46988
iraklitchedia <iraklitchedia@gmail.com>
2022-02-15 17:36:05 +0400

    CI/CD setup

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo cp /home/ubuntu/.env /home/ubuntu/mele-treasury-api-dev
-cd /home/ubuntu/mele-treasury-api-dev
+sudo cp /home/ubuntu/.env /home/ubuntu/mele-treasury-api
+cd /home/ubuntu/mele-treasury-api
 sudo npm run pm2:stop
 sudo npm run pm2:start
